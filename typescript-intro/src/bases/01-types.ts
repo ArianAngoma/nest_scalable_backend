5af7c0b2e2459b6858af313cc863a3d9c6d3e4f0
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-09-09 22:44:42 -0500

    Tipos de datos

--- typescript-intro/src/bases/01-types.ts
+++ typescript-intro/src/bases/01-types.ts
@@ -1,6 +1,17 @@
-export const name: string = 'Arian';
+export let name: string = 'Arian';
 
 export const age: number = 22;
 
 export const isAlive: boolean = true;
 
+export const tamplateString = `Esto es un string
+multilinea
+que puede tener
+" dobles
+' simple
+inyectar valores ${name}
+expresiones ${1 + 1}
+números ${age}
+booleanos ${isAlive}`;
+
+console.log(tamplateString);
